32f06bedbb7ba9d2d3b843c1d443835dbff89ce5
tobajetex <jetawotobajetex@gmail.com>
2024-03-09 10:08:55 +0100

    first commit

---
+++ src/components/nav.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/Navbar'
+const Navlink = () => {
+  return (
+    <Wrapper>
+      <div className="nav-center">
+        <span className="logo">MixMaster</span>
+        <div className="nav-links">
+          <NavLink to="/" className="nav-link">
+            Home
+          </NavLink>
+          <NavLink to="/About" className="nav-link">
+            About
+          </NavLink>
+          <NavLink to="/Newsletter" className="nav-link">
+            NewsLetter
+          </NavLink>
+        </div>
+      </div>
+    </Wrapper>
+  )
+}
+
+export default Navlink
